cba7f19bbf7732eba7647b2d8d9be204f05beb30
DhioAWP <dhioanugerah0@gmail.com>
2023-06-19 18:52:52 +0700

    first commit

---
+++ exe2.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video Bring Me The Horizon");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio Bring Me The Horizon");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Bring Me The Horizon");
+});
